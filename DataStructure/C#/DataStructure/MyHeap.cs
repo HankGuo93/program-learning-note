167a79e68687dc0833142441a54e4b1fccbfd14e
HankGuo <shank2288s@gmail.com>
2022-10-13 14:46:27 +0800

    feat : Implement MyHeap functions

--- DataStructure/C#/DataStructure/MyHeap.cs
+++ DataStructure/C#/DataStructure/MyHeap.cs
@@ -2,23 +2,78 @@
 
 namespace DataStructure
 {
-    public class MyHeap<T>
+    public class MyHeap
     {
-        public object this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int[] ArrayData { get; set; }
 
-        public void Insert(T value)
+        public int Counter;
+
+        public MyHeap(int lenth = 100)
+        {
+            ArrayData = new int[lenth];
+        }
+
+        public void Insert(int value)
+        {
+            ArrayData[Counter] = value;
+            Counter++;
+            Swin(Counter-1);
+        }
+
+        public int Extract()
+        {
+            var maxValue = ArrayData[0];
+            ArrayData[0] = 0;
+            Counter--;
+            MaxHeapify(0);
+            return maxValue;
+        }
+
+        public int Peek()
+        {
+            var maxValue = ArrayData[0];
+            return maxValue;
+        }
+
+        public int[] Sort()
         {
             throw new NotImplementedException();
         }
 
-        public void Remove(T value)
+        private void Swin(int index)
         {
-            throw new NotImplementedException();
+            while (index > 0 && ArrayData[(index - 1) / 2] < ArrayData[index])
+            {
+                var temp = ArrayData[(index - 1) / 2];
+                ArrayData[(index - 1) / 2] = ArrayData[index];
+                ArrayData[index] = temp;
+                index = (index - 1) / 2;
+            }
         }
 
-        public T[] Sort()
+        private void MaxHeapify(int index)
         {
-            throw new NotImplementedException();
+            var left = 2 * index;
+            var right = 2 * index + 1;
+            var largetNode = index;
+
+            if (left <= Counter && ArrayData[left] > ArrayData[largetNode])
+            {
+                largetNode = left;
+            }
+
+            if (right <= Counter && ArrayData[right] > ArrayData[largetNode])
+            {
+                largetNode = right;
+            }
+
+            if (largetNode != index)
+            {
+                var temp = ArrayData[index];
+                ArrayData[index] = ArrayData[largetNode];
+                ArrayData[largetNode] = temp;
+                MaxHeapify(largetNode);
+            }
         }
     }
 }
