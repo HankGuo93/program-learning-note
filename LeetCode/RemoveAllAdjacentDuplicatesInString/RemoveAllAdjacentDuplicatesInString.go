07b9496cbf8923e206075945064415acfcb1b6e3
HankGuo <shank2288s@gmail.com>
2022-11-12 23:12:30 +0800

    feat : [LeetCode] - Update RemoveAllAdjacentDuplicatesInString

--- LeetCode/RemoveAllAdjacentDuplicatesInString/RemoveAllAdjacentDuplicatesInString.go
+++ LeetCode/RemoveAllAdjacentDuplicatesInString/RemoveAllAdjacentDuplicatesInString.go
@@ -1,49 +1,15 @@
 package main
 
-import "strings"
+import (
+	"strings"
+)
 
 func removeDuplicates(s string) string {
-	sSplit := strings.Split(s, "")
-	if len(sSplit) == 1 {
-		return s
-	}
-	result := ""
-	for true {
-		if !innerRemoveDuplicates(s, sSplit, &result) {
-			break
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] == s[i+1] {
+			s = strings.Replace(s, string(s[i])+string(s[i]), "", -1)
+			i = -1
 		}
 	}
-	return result
-}
-
-func innerRemoveDuplicates(s string, sSplit []string, result *string) bool {
-	centralIndex := -1
-	for i := 0; i < len(sSplit)-1; i++ {
-		if sSplit[i] == sSplit[i+1] {
-			centralIndex = i
-			break
-		}
-	}
-	if centralIndex == -1 {
-		return true
-	}
-	duplicates := ""
-	leftIndex := centralIndex
-	rightIndex := centralIndex + 1
-	for true {
-		leftString := sSplit[leftIndex]
-		rightString := sSplit[rightIndex]
-		duplicates = leftString + duplicates + rightString
-		leftIndex -= 1
-		rightIndex += 1
-		if leftIndex < 0 || rightIndex > len(sSplit)-1 {
-			break
-		}
-		if sSplit[leftIndex] != sSplit[rightIndex] {
-			break
-		}
-	}
-	replaceString := strings.Replace(s, duplicates, "", -1)
-	result = &replaceString
-	return false
+	return s
 }
