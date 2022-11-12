bf4da81c838f085dd675321086f7b59d379fe76a
HankGuo <shank2288s@gmail.com>
2022-11-12 23:45:11 +0800

    feat : [LeetCode] - Update RemoveAllAdjacentDuplicatesInString

--- LeetCode/RemoveAllAdjacentDuplicatesInString/RemoveAllAdjacentDuplicatesInString.go
+++ LeetCode/RemoveAllAdjacentDuplicatesInString/RemoveAllAdjacentDuplicatesInString.go
@@ -4,6 +4,9 @@ import (
 	"strings"
 )
 
+//description : https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+//time complexity : O(n^2)
+//space complexity : O(1)
 func removeDuplicates(s string) string {
 	for i := 0; i < len(s)-1; i++ {
 		if s[i] == s[i+1] {
